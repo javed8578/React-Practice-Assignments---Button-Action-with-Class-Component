0c05af44ad865cb2600b9d4bbb107f599feb8394
javed857 <javedansari857@gmail.com>
2021-05-17 20:36:31 +0530

    neww

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
+    shoot(p) {
+		<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>;
 	};
 
     render() {
     	return(
     		<div id="main">
-				<button onClick={this.shoot} id="click"><p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p></button>
+				<button onClick={this.shoot} id="click">Button  </button>
 
 				{ /* Do not remove this main div!! */ }
     		</div>
