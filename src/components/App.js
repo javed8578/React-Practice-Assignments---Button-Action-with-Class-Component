38bb9ef091ecbef38b1d5a685f8ed41e02d4530c
javed857 <javedansari857@gmail.com>
2021-05-19 17:17:33 +0530

    jv

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import '../styles/App.css';
 
 class App extends React.Component {
 	handleClick(e){
-		this.shoot("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+		this.shoot(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>);
 	}
 
     render() {
     	return(
     		<div id="main">
-				<button onClick={this.shoot.bind(this)} id="click">Button  </button>
+				<button onClick={this.shoot.bind(this)} id="click">Button</button>
 
 				{ /* Do not remove this main div!! */ }
     		</div>
