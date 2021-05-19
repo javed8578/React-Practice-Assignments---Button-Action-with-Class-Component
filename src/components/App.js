4f3af942f323b5d1eea678cd6f4b5aa3933df4e1
javed857 <javedansari857@gmail.com>
2021-05-19 17:11:32 +0530

    Button Action with Class Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends React.Component {
-    shoot() {
-		<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-	};
+	handleClick(e){
+		this.shoot("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+	}
 
     render() {
     	return(
     		<div id="main">
-				<button onClick={this.shoot} id="click">Button  </button>
+				<button onClick={this.shoot.bind(this)} id="click">Button  </button>
 
 				{ /* Do not remove this main div!! */ }
     		</div>
